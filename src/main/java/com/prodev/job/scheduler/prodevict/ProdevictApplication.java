0690d1eaf241c2565c24e07d0c40f54ff2cf816a
mrkyaing <mr.kyaing7@gmail.com>
2022-04-17 19:55:55 +0630

    add log4j and logs to database

--- src/main/java/com/prodev/job/scheduler/prodevict/ProdevictApplication.java
+++ src/main/java/com/prodev/job/scheduler/prodevict/ProdevictApplication.java
@@ -1,5 +1,6 @@
 package com.prodev.job.scheduler.prodevict;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
--- src/main/java/com/prodev/job/scheduler/prodevict/Scheduler.java
+++ src/main/java/com/prodev/job/scheduler/prodevict/Scheduler.java
@@ -3,19 +3,25 @@ package com.prodev.job.scheduler.prodevict;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
-
+    private static Logger logger = Logger.getLogger(ProdevictApplication.class);
+    private void doSomething() {
+        logger.debug("im doing something");
+        logger.error("im doing something - error -");
+    }
     private BookingDAO bookingDAO;
     public Scheduler() {
         bookingDAO=new BookingDaoImpl();
     }
-    @Scheduled(cron = "0 59 23 1/1 * ?",zone = "Asia/Rangoon")//// at 23:59  every day
-
+    @Scheduled(cron = "0 55 19 1/1 * ?",zone = "Asia/Rangoon")//// at 23:59  every day
     public void updateAutomaticallyOverDueDateBooking() {
+        doSomething();
         LocalDateTime now=LocalDateTime.now();
         System.out.println("Java cron job expression:: " + now);
         try {
